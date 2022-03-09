d2ee0044c3cf205a43193d969fceacd9730edf9e
vtiari <vtiwari@paninian.com>
2022-03-09 19:44:10 +0530

    Kuch toh kiya hai

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 // IMPORT PAGES
 import Doctor from 'Pages/Doctor'
+import NewPrescription from "Pages/Doctor/NewPrescription"
 import Prescription from "Pages/Prescription"
 
 const App = () => {
@@ -12,7 +13,6 @@ const App = () => {
         <Routes>
           <Route path='/doctor' element={<Doctor />} >
             <Route path='new-prescription' element={<NewPrescription />} />
-
           </Route>
           <Route path='prescription' element={<Prescription />} />
         </Routes>
